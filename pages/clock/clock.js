810507fe3fc59fc3a1c4fb14723961403d2550c8
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-02-09 16:23:03 +0000

    clock: Added buttons

--- pages/clock/clock.js
+++ pages/clock/clock.js
@@ -1,22 +1,63 @@
-console.log("Hello world!")
+console.log("Hello world!");
 
 let count = 0;
+let clock;
 
-function tick() {
-    console.log("tick")
-    count++;
-    displayCount = document.getElementById("displayCount")
-    displayCount.innerText = `You've been looking at this page for ${count} seconds.`
+let frozen = false;
+let freezeCount = 0;
+let clickFrozen = false;
+let clickFrozenCount = false;
+
+function freezeClicking() {
+    if (clickFrozen) {
+        return false;
+    } else {
+        clickFrozen = true;
+        const freezeClickingButton = document.getElementById("freezeClicking");
+        freezeClickingButton.innerText = "Unfreeze clicking";
+    }
 }
 
-function createClock() {
-    let clock = setInterval(tick, 1000)
+function freeze() {
+    if (clickFrozen) {return false};
+    const freezeButton = document.getElementById("freeze");
+
+    if (frozen) {
+
+        frozen = false;
+        freezeButton.innerText = "Freeze time";
+        startClock();
+
+    } else {
+
+        frozen = true;
+        freezeButton.innerText = "Unfreeze time";
+        clearInterval(clock);
+
+        freezeCount++;
+        const displayFreezes = document.getElementById("displayFreezes");
+        displayFreezes.innerText = `You've frozen time ${freezeCount} times.`;
+
+    }
+
+    return true;
+}
+
+function tick() {
+    console.log("tick");
+    count++;
+    const displayTime = document.getElementById("displayTime");
+    displayTime.innerText = `You've been looking at this page for ${count} seconds.`;
+}
+
+function startClock() {
+    clock = setInterval(tick, 1000);
 }
 
 function onLoad() {
-    console.log("The page has loaded!")
-    createClock()
+    console.log("The page has loaded!");
+    startClock();
 
 }
 
-document.addEventListener("DOMContentLoaded", onLoad)
+document.addEventListener("DOMContentLoaded", onLoad);
