054593f0b89952a7f55cbbbc19428263378fe5e6
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-03-06 20:50:25 +0000

    New page: Rules for Robots

---
+++ pages/rules-for-robots/rules-for-robots.js
@@ -0,0 +1,57 @@
+console.log("Hello world!");
+
+function isRedirect() {
+    if (getParameter("url")) {
+        // window.open(`${getParameter("url")}/robots.txt`, "_self");
+        window.location.replace(`${getParameter("url")}/robots.txt`, "_self");
+        return true;
+    } else {
+        removeParameter("url");
+        return false;
+    }
+}
+
+function removeParameter(key) {
+    // get the current url
+    const url = new URL(window.location.href);
+
+    // delete the parameter
+    url.searchParams.delete(key);
+}
+
+function getParameter(key) {
+    // get the current url
+    const url = new URL(window.location.href);
+
+    // get the parameter
+    const parameter = url.searchParams.get(key);
+    return parameter;
+}
+
+function setParameter(key, value) {
+    // get the current url
+    const url = new URL(window.location.href);
+
+    // set the parameter
+    url.searchParams.set(key, value);
+
+    // who knows what this does
+    window.history.pushState({}, "", url);
+
+    return;
+}
+
+function onLoad() {
+    console.log("Page loaded!");
+    if (!isRedirect()) {
+        const field = document.getElementById("field");
+        field.addEventListener("keydown", (event) => {
+            if (event.key === "Enter") {
+                setParameter("url", field.value);
+                isRedirect();
+            }
+        });
+    }
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
