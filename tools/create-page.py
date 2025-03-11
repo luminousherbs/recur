1288c26b29a5e34ab88005a1113ef9c2ae1e5a2a
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-03-11 19:55:15 +0000

    Unbreak tools

--- tools/create-page.py
+++ tools/create-page.py
@@ -4,21 +4,22 @@ import sys
 def create_page(name: str) -> None:
     name = name.lower()
     # create the requested folder
-    os.makedirs(f"/things/{name}")
+    # os.makedirs(f"/things/{name}")
+    os.makedirs(f"things/{name}")
 
-    html_template = open("/tools/template.html")
-    with open(f"/things/{name}/index.html", "x") as index:
+    html_template = open("tools/template.html")
+    with open(f"things/{name}/index.html", "x") as index:
         content = html_template.read()
         # replace variable references with the actual variables
         content = content.format(page_name = name)
         index.write(content)
     
-    js_template = open("/tools/template.js")
-    with open(f"/things/{name}/{name}.js", "x") as scripts:
+    js_template = open("tools/template.js")
+    with open(f"things/{name}/{name}.js", "x") as scripts:
         content = js_template.read()
         scripts.write(content)
 
-    with open("index.html", "r+") as main:
+    with open("things/index.html", "r+") as main:
         content = main.read()
         list_end = content.index("</ul>") - 4
         # do not edit the ancient runes
