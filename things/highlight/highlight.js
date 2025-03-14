ccfe299f0369797c75803638cfa6cf8e6cbfac57
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-03-14 21:30:36 +0000

    Create /things/highlight

---
+++ things/highlight/highlight.js
@@ -0,0 +1,50 @@
+console.log("Hello world!");
+colors = new Map([
+    ["const", "lightgreen"],
+    ["let", "lightgreen"],
+    ["if", "lightgreen"],
+    ["else", "lightgreen"],
+    ["of", "lightgreen"],
+    ["=", "white"],
+    ["==", "white"],
+    ["===", "white"],
+])
+
+let input, output;
+
+function updateText(text) {
+    let newText = "";
+    for (let word of text.replaceAll("\n", " <br> ").split(" ")) {
+        replacement = colors.get(word);
+        if (replacement) {
+            newText += `<span style="color: ${replacement}">${word}</span>`;
+        } else if (/^([0-9])+$/.test(word)) {
+            newText += `<span style="color: yellow">${word}</span>`;
+        } else if ((word[0] === "\"") && (word[word.length - 1] === "\"")) {
+            newText += `<span style="color: indianred">${word}</span>`;
+        } else {
+            newText += `<span style="color: lightblue">${word}</span>`;
+        }
+        newText += " ";
+    }
+
+    // newText = newText.replaceAll("\n", " <br>");
+    output.innerHTML = newText;
+}
+
+function onInput() {
+    updateText(input.value);
+}
+
+function onLoad() {
+    console.log("Page loaded!");
+
+    // define elements
+    input = document.getElementById("input");
+    output = document.getElementById("output");
+    input.addEventListener("input", onInput)
+
+    updateText(input.value);
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
