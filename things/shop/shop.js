c50f5b7b0e57356160fcb41e9541a8eddbe131ae
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-03-20 21:27:22 +0000

    Create new page things/shop

---
+++ things/shop/shop.js
@@ -0,0 +1,62 @@
+console.log("Hello world!");
+
+const prices = new Map([
+    ["Strength Potion", 5],
+    ["Sword", 20],
+    ["Gem", 50],
+])
+
+function getCoins() {
+    return localStorage.getItem("coins") || 0;
+}
+
+function buy(item) {
+    let coins = getCoins()
+    if (coins < prices.get(item)) return false;
+    coins -= prices.get(item);
+    localStorage.setItem("coins", coins);
+    localStorage.setItem(item, Number(localStorage.getItem(item)) + 1);
+    updateDisplay();
+}
+
+function updateDisplay() {
+    output.innerText = `You have ${localStorage.getItem("coins") || 0} coins.`
+    itemsDisplay.innerHTML = "";
+    prices.forEach(function(value, key) {
+        console.log(`Checking ${key}: ${!!(localStorage.getItem(key))}`)
+        if (localStorage.getItem(key)) itemsDisplay.innerHTML += `${key}: ${localStorage.getItem(key)} <br>`
+    })
+    updateAffordable();
+}
+
+function updateAffordable() {
+    prices.forEach(function(value, key) {
+        if (value > getCoins()) {
+            console.log(key, document.getElementById(key));
+            document.getElementById(key).children[2].firstChild.disabled = true;
+        } else {
+            document.getElementById(key).children[2].firstChild.disabled = false;
+        }
+    })
+}
+
+function addCoin() {
+    let coins = getCoins();
+    coins++;
+    localStorage.setItem("coins", coins);
+    updateDisplay();
+}
+
+function onLoad() {
+    console.log("Page loaded!");
+
+    // define elements
+    const output = document.getElementById("output");
+    const itemsDisplay = document.getElementById("itemsDisplay")
+    const getCoin = document.getElementById("getCoin");
+    getCoin.addEventListener("click", addCoin);
+    updateDisplay();
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
+
