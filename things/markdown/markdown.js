d8e50404823dd787ccfbda455a11165015d26880
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-03-24 21:09:27 +0000

    Fix markdown

--- things/markdown/markdown.js
+++ things/markdown/markdown.js
@@ -1,34 +1,86 @@
 console.log("Hello world!");
 let input, output;
-const syntaxes = new Map([
+
+const toggleables = new Map([
+    ["**", ["<strong>", "</strong>"]],
+    ["*", ["<em>", "</em>"]],
+    ["~~", ["<del>", "</del>"]],
+    ["```", ["<pre><code>", "</pre></code>"]],
+    ["`", ["<code>", "</code>"]],
+])
+
+const lineEffects = new Map([
     ["## ", ["<h2>", "</h2>"]],
     ["### ", ["<h3>", "</h3>"]],
     ["# ", ["<h1>", "</h1>"]],
-    ["** ", ["<strong>", "</strong>"]],
-    ["* ", ["<em>", "</em>"]],
     ["> ", ["<aside>", "</aside>"]],
     ["---", ["<hr>", ""]],
-    ["``` ", ["<pre><code>", "</pre></code>"]],
-    ["` ", ["<code>", "</code>"]],
 ])
 
-function onInput() {
-    let lines = input.value.split("\n");
-    let counter = 0
-    // pyramid of doom
-    for (let l of lines) {
-        for (let s of syntaxes.keys()) {
-            if (l.includes(s)) {
-                l = l.replaceAll(s, syntaxes.get(s)[0]);
-                l += syntaxes.get(s)[1];
-                // console.log(s)
-                console.log(l);
-                lines[counter] = l + "\n";
+const defaultInput = `## Welcome to Markdown
+Your text can be *italic* or **bold** or ***both***, and you can format \`code\` as well.
+~~You can create lists and tables, too!~~ Lists and tables are not supported.
+### This is a subheading,
+And here are some horizontal lines:
+---
+---
+---
+> Pretty cool, huh?`;
+
+function replaceToggleables(text) {
+    // loop for each tag
+    for (let t of toggleables.keys()) {
+        let opened = false;
+        // while this markdown tag is still present,
+        while (text.includes(t)) {
+            if (opened) {
+                // if the tag is already opened, replace the next markdown syntax with a html closing tag,
+                // e.g. replace * with </em>
+                text = text.replace(t, toggleables.get(t)[1]);
+                opened = false;
+            } else {
+                // if the tag isn't already opened, replace the next markdown syntax with a html opening tag,
+                // e.g. replace * with <em>
+                text = text.replace(t, toggleables.get(t)[0]);
+                opened = true;
             }
         }
-        counter += 1;
     }
-    output.innerHTML = lines.join("\n");
+    return text;
+}
+
+function replaceLineEffects(text) {
+    lines = text.split("\n");
+    let newText = "";
+    for (let l of lines) {
+        newl = l;
+        for (let e of lineEffects.keys()) {
+            if (l.startsWith(e)) {
+                // surround the line with the relevant opening and closing tags
+                newl = `${lineEffects.get(e)[0]}${l.replace(e, "")}${lineEffects.get(e)[1]}`
+            }
+        }
+        newText += newl;
+        // weird but functional
+        newText += (newl === l ? "<br>": "");
+    }
+    return newText;
+}
+
+function onInput() {
+
+    // set the height of the box to the height of the content
+    input.style.height = "auto";
+    input.style.height = input.scrollHeight + 'px';
+
+    // convert markdown to html
+    text = replaceLineEffects(input.value);
+    text = text.replaceAll("\n", "<br>"); // html sucks
+    text = replaceToggleables(text);
+
+    // set the html
+    output.innerHTML = text;
+
 }
 
 
@@ -41,6 +93,10 @@ function onLoad() {
     
     // listen for events
     input.addEventListener("input", onInput);
+
+    // set default value
+    input.value = defaultInput;
+    onInput();
 }
 
 document.addEventListener("DOMContentLoaded", onLoad);
