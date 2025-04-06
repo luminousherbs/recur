da2d9a4f6f8d29eb4ac0c0fff511fb4566ba0b00
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-04-06 16:18:04 +0100

    Rename even more things

--- tools/create-thing.py
+++ tools/create-thing.py
@@ -4,7 +4,6 @@ import sys
 def create_page(filename: str) -> None:
     titlename = filename.title().replace("-", " ")
     # create the requested folder
-    # os.makedirs(f"/things/{name}")
     os.makedirs(f"things/{filename}")
 
     html_template = open("tools/template.html")
@@ -15,16 +14,13 @@ def create_page(filename: str) -> None:
         index.write(content)
     
     js_template = open("tools/template.js")
-    with open(f"things/{filename}/{filename}.js", "x") as scripts:
+    with open(f"things/{filename}/script.js", "x") as script:
         content = js_template.read()
-        scripts.write(content)
+        script.write(content)
 
     with open("things/index.html", "r+") as main:
         content = main.read()
         list_end = content.index("</ul>") - 4
-        # do not edit the ancient runes
-        content = content[:list_end] + f"        <li><a href=\"/things/{filename}/\">{titlename}</a></li>\n" + content[list_end:]
-        # print(content)
         main.seek(0)
         main.write(content)
 
