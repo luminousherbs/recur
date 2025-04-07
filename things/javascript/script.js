f44d9b82be01a73b5607946c64775dde10c2c7ea
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-04-07 21:48:07 +0100

    Create things/javascript/

---
+++ things/javascript/script.js
@@ -0,0 +1,30 @@
+
+
+function onInput() {
+    const inputText = inputField.value;
+    let outputText = []
+    let originalLog = console.log
+
+    try {
+        // hijacking console.log isn't ideal but it's the best i could come up with
+        console.log = (...args) => {
+            outputText.push(args.join(" "));
+        }
+
+        eval(inputText);
+    } catch (error) {
+        outputText.push(`Line ${error.lineNumber || 0}: ${error.name}: ${error.message}`);
+    } finally {
+        console.log = originalLog;
+    }
+    outputField.innerText = outputText.join("\n");
+
+}
+
+function onLoad() {
+    // define elements
+    const inputField = document.getElementById("inputField");
+    const outputField = document.getElementById("outputField");
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
