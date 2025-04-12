7c953f4ae7f1b198c4c459acaf4749fc3ece55db
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-04-12 11:31:22 +0100

    Fix

---
+++ things/appear/script.js
@@ -0,0 +1,53 @@
+console.log("Hello world!");
+
+let index = 0;
+let words;
+let intervalSpeed = 200;
+let intervalId;
+let slider;
+
+async function getText(url) {
+    const response = await fetch(url);
+    const data = await response.text();
+    return data;
+}
+
+// i wanted to include a speed slider but html sucks :(
+// function slide() {
+//     intervalSpeed = slider.value;
+//     clearInterval(intervalId);
+//     setTimeout(function(){}, intervalSpeed);
+//     intervalId = startRepeat(intervalSpeed);
+// }
+
+function startRepeat(speed) {
+    let interval = setInterval(() => {addWord(main, interval)}, speed);
+}
+
+function addWord(container, interval) {
+    if (index >= words.length) {
+        clearInterval(interval);
+        return;
+    }
+    container.innerHTML += "&#32;" + words[index];
+
+    // slider = document.getElementById("slider")
+    // if (slider) {
+    //     slider.addEventListener("input", slide)
+    // }
+
+    index++;
+}
+
+function onLoad() {
+    console.log("Page loaded!");
+    getText("words.txt").then((result) => {
+        words = result.split("\n");
+    })
+
+    // define elements
+    const main = document.getElementById("main");
+    intervalId = startRepeat(intervalSpeed)
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
