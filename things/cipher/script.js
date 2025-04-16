ed5ea980d49daa26c4664c5dc05e7b32fc6e0712
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-04-16 20:32:03 +0100

    Create things/cipher

---
+++ things/cipher/script.js
@@ -0,0 +1,71 @@
+console.log("Hello world!");
+let forward = true;
+
+// very silly solution
+const shiftReference = "abcdefghijklmnopqrstuvwxyzaABCDEFGHIJKLMNOPQRSTUVWXYZA";
+
+function shiftOne(str) {
+    let newstr = "";
+    for (let s of str) {
+        if (shiftReference.includes(s)) {
+            newstr += shiftReference[shiftReference.indexOf(s) + 1]
+        } else {
+            newstr += s
+        }
+    }
+    return newstr;
+}
+
+function shiftMinusOne(str) {
+    let newstr = "";
+    for (let s of str) {
+        if (shiftReference.includes(s)) {
+            newstr += shiftReference[shiftReference.lastIndexOf(s) - 1]
+        } else {
+            newstr += s
+        }
+    }
+    return newstr;
+}
+
+function caesarShift(str, shift) {
+    for (let i = 0; i < Math.abs(shift); i++) {
+        str = (Math.abs(shift) === shift ? shiftOne: shiftMinusOne)(str)
+    }
+    return str;
+}
+
+
+function onSlideInput() {
+    amountField.innerText = shiftField.value;
+    if (forward) {
+        onForwardInput()
+    } else {
+        onBackwardInput()
+    }
+}
+
+function onForwardInput() {
+    forward = true;
+    encodedField.value = caesarShift(decodedField.value, +shiftField.value);
+}
+
+function onBackwardInput() {
+    forward = false;
+    decodedField.value = caesarShift(encodedField.value, -+shiftField.value);
+}
+
+function onLoad() {
+    console.log("Page loaded!");
+
+    // define elements
+    const decodedField = document.getElementById("decodedField");
+    const encodedField = document.getElementById("encodedField");
+    const shiftField   = document.getElementById("shiftField"  );
+
+    decodedField.addEventListener("input", onForwardInput);
+    encodedField.addEventListener("input", onBackwardInput);
+    shiftField  .addEventListener("input", onSlideInput);
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
