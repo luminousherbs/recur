60ffc19e4399263a9013bb850177534f607bad11
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-04-30 21:06:54 +0100

    Create things/rich-people-only

---
+++ things/rich-people-only/script.js
@@ -0,0 +1,51 @@
+console.log("Hello world!");
+
+function createPoorText() {
+
+    const poorText = document.createDocumentFragment();
+
+    const poorTitle = document.createElement("h2");
+    poorTitle.textContent = "You are not rich."
+
+    const poorDescription = document.createElement("p");
+    poorDescription.textContent = "Come back when you have at least 100 coins."
+
+    poorText.append(poorTitle, poorDescription);
+    return poorText;
+
+}
+
+function createRichText() {
+
+    const richText = document.createDocumentFragment();
+
+    const richTitle = document.createElement("h2");
+    richTitle.textContent = "Congratulations! You are rich."
+
+    const richDescription = document.createElement("p");
+    richDescription.textContent = "You may bask in your wealth and glory."
+
+    richText.append(richTitle, richDescription);
+    return richText;
+
+}
+
+function getCoins() {
+    return +localStorage.getItem("coins");
+}
+
+function isRich() {
+    return getCoins() >= 100;
+}
+
+function onLoad() {
+    console.log("Page loaded!");
+
+    text = isRich() ? createRichText() : createPoorText();
+
+    // define elements
+    const ruleEnd = document.getElementById("rule-end");
+    ruleEnd.parentNode.insertBefore(text, ruleEnd);
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
