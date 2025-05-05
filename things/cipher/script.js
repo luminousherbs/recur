0d896dfbc4b0ae251963cf432c8863cbf1977a0f
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-05-05 12:28:54 +0100

    Add buttons
    I need a better build process

--- things/cipher/script.js
+++ things/cipher/script.js
@@ -1,6 +1,34 @@
 console.log("Hello world!");
 let forward = true;
 
+const pageLocation = "things/cipher";
+
+function downloadFile(filepath) {
+    const a = document.createElement("a");
+    a.href = filepath;
+    a.download = "";
+    a.hidden = true;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+function edit() {
+    window.location.href = `https://github.com/luminousherbs/luminousherbs.github.io/tree/main/${pageLocation}/`;
+}
+
+function share() {
+    navigator.share({url: window.location.href});
+}
+
+function downloadHTML() {
+    downloadFile("./index.html");
+}
+
+function downloadJavaScript() {
+    downloadFile("./script.js");
+}
+
 // very silly solution
 const shiftReference = "abcdefghijklmnopqrstuvwxyzaABCDEFGHIJKLMNOPQRSTUVWXYZA";
 
