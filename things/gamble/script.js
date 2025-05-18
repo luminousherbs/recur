e64c08b37dac20ab6826f2d962a8cd593b1e31b1
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-05-18 09:55:45 +0100

    Create things/gamble

--- things/gamble/script.js
+++ things/gamble/script.js
@@ -2,6 +2,8 @@ console.log("Hello world!");
 
 const pageLocation = "things/gamble";
 
+let gambleButton, moneyField, coinsField;
+
 function downloadFile(filepath) {
     const a = document.createElement("a");
     a.href = filepath;
@@ -28,8 +30,77 @@ function downloadJavaScript() {
     downloadFile("./script.js");
 }
 
+function getCoins() {
+    return +localStorage.getItem("coins") ?? 0;
+}
+
+function onClick() {
+    let gambledMoney = +moneyField.value;
+    let coins = getCoins();
+    coins += gamble(gambledMoney);
+    localStorage.coins = coins;
+    updateDisplay();
+}
+
+function gamble(amount) {
+
+    // reject unnatural numbers
+    if (
+        isNaN(amount)
+        || 
+        amount % 1 !== 0
+        ||
+        amount <= 0
+    ) {
+        alert("must be a natural number");
+        return 0;
+    }
+
+    // reject gambling more than the user has
+    if (amount > getCoins()) {
+        alert("you don't have that much money");
+        return 0;
+    }
+
+    // if we get this far then the user gambled an acceptable amount of money
+
+    const luck = Math.random();
+
+    if (luck < (1/144)) {
+        // 1/144 chance to win 100x your wager
+        alert(`Congrats! You won ${amount * 100} coins.`);
+        return amount * 100;
+
+    } else if (luck < (1/12)) {
+        // 11/144 chance to win 10x your wager
+        alert(`Congrats! You won ${amount * 10} coins.`);
+        return amount * 10;
+
+    } else {
+        // 132/144 chance to win 0x your wager
+        alert(`Congrats! You didn't win this time.`);
+        return -amount;
+
+    }
+}
+
+function updateDisplay() {
+    coinsField.innerText = getCoins();
+}
+
 function onLoad() {
     console.log("Page loaded!");
+
+    // define elements
+    gambleButton = document.getElementById("gamble");
+    moneyField = document.getElementById("money");
+    coinsField = document.getElementById("coins");
+
+    // define listeners
+    gambleButton.addEventListener("click", onClick);
+
+    // initialize
+    updateDisplay();
 }
 
 document.addEventListener("DOMContentLoaded", onLoad);
