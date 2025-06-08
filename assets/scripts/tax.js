67d658d07e71834decf13e4cddae178bd0233c19
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-06-08 10:27:31 +0100

    Add tax

---
+++ assets/scripts/tax.js
@@ -0,0 +1,27 @@
+function year() {
+    // Date.now() -> number of milliseconds since 1970
+    return 1970 + (Date.now() / (1000 * 60 * 60 * 24 * 365));
+}
+
+function nextMarch() {
+    const currentYear = year();
+    const currentMonth = 12 * (currentYear % 1) + 1
+    return `March 31st, ${
+        Math.floor(currentYear) + (
+            (currentMonth > 3) ? 1 : 0
+        )
+    }`;
+}
+
+const message = `You have too much money! You must submit your tax return by ${nextMarch()} or face prosecution.`;
+
+
+if (
+    (+localStorage.coins >= 250)
+    &&
+    (Math.random() < (1 / 5))
+) {
+    if (window.confirm(message)) {
+        window.location.href = "/things/tax/";
+    }
+}
---
+++ things/tax/script.js
@@ -0,0 +1,48 @@
+console.log("Hello world!");
+
+const taxRate = 0.2;
+
+function coins() {
+    return +localStorage.coins;
+}
+
+function calculateTax(money) {
+    return (
+        ((money * (1 - taxRate)) < 250) ? (money * taxRate) : (money - 249)
+    );
+}
+
+function richText() {
+    return `You currently have ${coins()} coins, which is too many. Press the button below to submit your tax returns and pay ${calculateTax(coins())} coins to the government.`;
+}
+
+function poorText() {
+    return `You don't need to pay tax.`;
+}
+
+function onLoad() {
+    console.log("Page loaded!");
+
+    // define elements
+    const messageBox = document.getElementById("message");
+    const button = document.getElementById("submit");
+
+    // logic
+    if (coins() >= 250) {
+        messageBox.innerText = richText();
+        button.disabled = false;
+    } else {
+        messageBox.innerText = poorText();
+        button.disabled = true;
+    }
+
+    button.addEventListener("click", function() {
+        if (coins() >= 250) {
+            localStorage.coins = coins() - calculateTax(coins());
+            alert("Your tax has been paid successfully.");
+        }
+        window.location.reload();
+    })
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
