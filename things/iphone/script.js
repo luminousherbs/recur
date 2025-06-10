2ed61d09d39693f00907c6a5477d3239628a730b
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-06-10 20:31:36 +0100

    things/iphone: add dual sorting (resolves #31)

--- things/iphone/script.js
+++ things/iphone/script.js
@@ -1,7 +1,5 @@
 console.log(window.location.pathname);
 
-let phoneContainer;
-
 async function getJSON(url) {
     const response = await fetch(url);
     const data = await response.json();
@@ -17,17 +15,17 @@ function sortBy(collection, key) {
     return (objects);
 }
 
-function displaySorted(collection, key) {
+function displaySorted(collection, key1, key2) {
     // clear page
     phoneContainer.innerHTML = "";
 
-    for (let phone of sortBy(collection, key)) {
+    for (let phone of sortByTwoKeys(collection, key1, key2)) {
 
         const phoneDiv = document.createElement("div");
         phoneDiv.dataset.phoneId = phone[0];
 
         const phoneHeading = document.createElement("h1");
-        phoneHeading.textContent = `iPhone ${phone[0]}: ${phone[1][key]}`;
+        phoneHeading.textContent = `iPhone ${phone[0]}: ${phone[1][key1]}, ${phone[1][key2]}`;
 
         phoneDiv.appendChild(phoneHeading);
         phoneContainer.appendChild(phoneDiv);
@@ -35,16 +33,34 @@ function displaySorted(collection, key) {
     }
 }
 
+function sortByTwoKeys(collection, key1, key2) {
+    const objects = Object.entries(collection);
+
+    // sort by key (descending order)
+    objects.sort(
+        (a, b) => 
+        (b[1][key1] + b[1][key2]) - (a[1][key1] + a[1][key2])
+    );
+
+    return (objects);
+}
+
 // define elements
-phoneContainer = document.getElementById("phones");
-let stats;
+const phoneContainer = document.getElementById("phones");
+const field1 = document.getElementById("select1");
+const field2 = document.getElementById("select2");
 
 // get stats
+let stats;
 getJSON("stats.json").then((res) => {
     stats = res;
-    displaySorted(stats, "price");
+    displaySorted(stats, field1.value, field2.value);
 })
 
-document.getElementById("select").addEventListener("input", function() {
-    displaySorted(stats, document.getElementById("select").value)
+field1.addEventListener("input", function() {
+    displaySorted(stats, field1.value, field2.value);
+})
+
+field2.addEventListener("input", function() {
+    displaySorted(stats, field1.value, field2.value);
 })
